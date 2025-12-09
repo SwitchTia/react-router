e290ad6722b2a5d441e982e4beb94994c333e248
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-09 23:21:55 +0100

    Feat: content fill, debug

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,43 @@
 import AppHeader from "../components/AppHeader";
 
 export default function AboutUs() {
-  return (
-    <>
-      <h1>About us</h1>
-    </>
-  );
+    return (
+        <>
+            <div className="container">
+                <h1>About us</h1>
+            <hr />
+            <p className="py-40">
+                At ShopWithUs, we believe shopping should be more than just a transaction. It should be an experience. That’s why we focus on:
+            </p>
+
+            <hr />
+
+            <div className="py-40">
+                <ul>
+                    <li> Premium Quality
+                        Every product in our store is hand-selected to ensure durability, reliability, and great value.</li>
+
+                    <li> Customer-First Service
+                        Your satisfaction matters to us. Our support team is always here to help you with questions, returns, or recommendations.
+                    </li>
+
+                    <li> Fast & Secure Delivery
+                        With advanced logistics partners and secure payment systems, we make sure your orders arrive safely and on time.
+                    </li>
+                    <li> A Growing Community</li>
+                </ul>
+            </div>
+
+            <hr />
+
+            <p className="py-40">
+                We’re more than a store — we’re a community of shoppers who value convenience, style, and smart choices.
+
+                Whether you’re looking for the latest trends, everyday essentials, or unique items you won’t find elsewhere, NovaCart is here to help you discover it all.
+
+                Thank you for choosing ShopWithUs — where quality meets convenience.
+            </p>
+            </div>
+        </>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,39 @@
 
 
 export default function Home() {
-  return (
-    <>
-      <section>
-        <h1>Home</h1>
-      </section>
+    return (
+        <>
+            <div className="container">
+                <div className="text-center py-40">
+                    <h1>Welcome to ShopWithUs</h1>
+                    <hr />
+                </div>
 
-      <section>
-        <h2>Contact us</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A aspernatur
-          autem velit quidem officiis eos eius obcaecati quia omnis.
-        </p>
-      
-      </section>
-    </>
-  );
+                <div>
+                    <h3 className="py-40">
+                        Welcome to ShopWithUs, your trusted online destination for quality products and a seamless shopping experience.
+                        Founded with a simple mission — to make online shopping easier, faster, and more enjoyable — NovaCart brings together a curated selection of items from trusted brands and emerging creators around the world.
+                    </h3>
+                </div>
+
+
+
+                <div className="py-40">
+                    <h4>What We Offer:</h4>
+                    <hr />
+                    <p className="py-20">At ShopWithUS, we handpick every item to ensure our customers get a curated selection of:</p>
+                    <div className="py-20">
+                        <ul>
+                            <li>Fashion & Apparel: Trendy clothing, accessories, and seasonal collections</li>
+                            <li>Electronics & Gadgets: Smart devices, accessories, and everyday tech</li>
+                            <li>Home & Lifestyle: Decor, organization, kitchenware, and comfort essentials</li>
+                            <li>Beauty & Wellness: Skincare, grooming, and personal-care favorites</li>
+                            <li>Special Deals: Weekly promotions, bundle offers, and exclusive discounts</li>
+                        </ul>
+                    </div>
+                </div>
+            </div >
+
+        </>
+    );
 }
